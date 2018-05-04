f14c9fdc23ea4a2a8b2ce53c052948349610b668
ivanf <ivanf@10.128.68.56>
2018-05-04 08:09:42 -0300

    Classe encomenda criada, Classe Cliente criada

--- ProjetoLP3/src/Telas/NovoCliente.java
+++ ProjetoLP3/src/Telas/NovoCliente.java
@@ -69,6 +69,11 @@ public class NovoCliente extends javax.swing.JFrame {
         jTextField6.setEditable(false);
 
         jButton3.setText("Confirmar");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
 
         jButton4.setText("Cancelar");
         jButton4.addActionListener(new java.awt.event.ActionListener() {
@@ -170,8 +175,12 @@ public class NovoCliente extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
-        System.exit(WIDTH);
+        dispose();
     }//GEN-LAST:event_jButton4ActionPerformed
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        dispose();
+    }//GEN-LAST:event_jButton3ActionPerformed
 
     /**
      * @param args the command line arguments
